9c76ab31209974fa266f3938143de7c79350f99d
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-03-09 23:38:19 -0300

    Add files via upload

---
+++ exercicio10/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int N = 4;
+        double[,] A = new double[N, N];
+
+        Console.WriteLine("Digite os valores da matriz 4x4:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write($"A[{i},{j}]: ");
+                A[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double somaA = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                somaA += A[i, j];
+            }
+        }
+
+        double somaB = 0;
+        for (int i = 2; i < 4; i++)
+        {
+            for (int j = 2; j < 4; j++)
+            {
+                somaB += A[i, j];
+            }
+        }
+
+        double somaC = 0;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (i > j)
+                {
+                    somaC += A[i, j];
+                }
+            }
+        }
+
+        double somaD = 0;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (i < j) 
+                {
+                    somaD += A[i, j];
+                }
+            }
+        }
+
+        Console.WriteLine($"\nSoma (a) - canto sup. esquerdo 2x2: {somaA}");
+        Console.WriteLine($"Soma (b) - canto inf. direito 2x2: {somaB}");
+        Console.WriteLine($"Soma (c) - abaixo da diagonal principal: {somaC}");
+        Console.WriteLine($"Soma (d) - acima da diagonal principal: {somaD}");
+    }
+}
---
+++ exercicio9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        const int NUM_JUIZES = 6;
+        double[] notas = new double[NUM_JUIZES];
+
+        for (int i = 0; i < NUM_JUIZES; i++)
+        {
+            Console.Write($"Digite a nota do juiz {i + 1} (0 a 10): ");
+            while (!double.TryParse(Console.ReadLine(), out notas[i]) || notas[i] < 0 || notas[i] > 10)
+            {
+                Console.Write("Nota inválida! Digite um valor entre 0 e 10: ");
+            }
+        }
+
+        Array.Sort(notas);
+
+        double notaFinal = notas.Skip(1).Take(NUM_JUIZES - 2).Sum();
+
+        Console.WriteLine($"A nota final do atleta é: {notaFinal:F2}");
+    }
+}
